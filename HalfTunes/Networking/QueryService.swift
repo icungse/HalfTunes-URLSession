8018b5d1adc66f1b889330ad1910b390ffc64048
Icung <icung@Icungs-MacBook-Air.local>
2023-06-26 00:06:00 +0700

    DataTask and DownloadTask

--- HalfTunes/Networking/QueryService.swift
+++ HalfTunes/Networking/QueryService.swift
@@ -37,12 +37,14 @@ class QueryService {
   //
   // MARK: - Constants
   //
-  // TODO 1
+  
+  let defaultSession = URLSession(configuration: .default)
   
   //
   // MARK: - Variables And Properties
   //
-  // TODO 2
+  
+  var dataTask: URLSessionDataTask?
   var errorMessage = ""
   var tracks: [Track] = []
   
@@ -56,10 +58,33 @@ class QueryService {
   // MARK: - Internal Methods
   //
   func getSearchResults(searchTerm: String, completion: @escaping QueryResult) {
-    // TODO 3
-    DispatchQueue.main.async {
-      completion(self.tracks, self.errorMessage)
+    dataTask?.cancel()
+    
+    if var urlComponents = URLComponents(string: "https://itunes.apple.com/search") {
+      urlComponents.query = "media=music&entity=song&term=\(searchTerm)"
+      
+      guard let url = urlComponents.url else {
+        return
+      }
+      
+      dataTask = defaultSession.dataTask(with: url) { [weak self] data, response, error in
+        defer {
+          self?.dataTask = nil
+        }
+        
+        if let error = error {
+          self?.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+        } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
+          self?.updateSearchResults(data)
+          
+          DispatchQueue.main.async {
+            completion(self?.tracks, self?.errorMessage ?? "")
+          }
+        }
+      }
     }
+    
+    dataTask?.resume()
   }
   
   //
