8610b691a5e45defb4f90e14c57ef6e6c5990b0e
Icung <icung@Icungs-MacBook-Air.local>
2023-06-26 00:33:29 +0700

    Saving and Playing the Track

--- HalfTunes/View Controllers/SearchViewController.swift
+++ HalfTunes/View Controllers/SearchViewController.swift
@@ -232,6 +232,30 @@ extension SearchViewController: TrackCellDelegate {
 
 extension SearchViewController: URLSessionDownloadDelegate {
   func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-    print("Finished downloading to \(location).")
+    guard let sourceURL = downloadTask.originalRequest?.url else {
+      return
+    }
+    
+    let download = downloadService.activeDonwloads[sourceURL]
+    downloadService.activeDonwloads[sourceURL] = nil
+    
+    let destinationURL = localFilePath(for: sourceURL)
+    print(destinationURL)
+    
+    let fileManager = FileManager.default
+    try? fileManager.removeItem(at: destinationURL)
+    
+    do {
+      try fileManager.copyItem(at: location, to: destinationURL)
+      download?.track.downloaded = true
+    } catch let error {
+      print("Could not copy file to disk: \(error.localizedDescription)")
+    }
+    
+    if let index = download?.track.index {
+      DispatchQueue.main.async { [weak self] in
+        self?.tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .none)
+      }
+    }
   }
 }
